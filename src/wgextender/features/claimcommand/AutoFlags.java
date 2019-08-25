a4902a51a3250c2abdc0773e8cc0f58612763b7e
zenit <alexandrage@users.noreply.github.com>
2019-08-25 11:28:50 +0300

    up

--- src/wgextender/features/claimcommand/AutoFlags.java
+++ src/wgextender/features/claimcommand/AutoFlags.java
@@ -26,16 +26,7 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Server;
 import org.bukkit.World;
-import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.conversations.Conversation;
-import org.bukkit.conversations.ConversationAbandonedEvent;
-import org.bukkit.permissions.Permission;
-import org.bukkit.permissions.PermissionAttachment;
-import org.bukkit.permissions.PermissionAttachmentInfo;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.util.Vector;
 
 import com.sk89q.minecraft.util.commands.Command;
 import com.sk89q.minecraft.util.commands.CommandContext;
@@ -69,8 +60,6 @@ import com.sk89q.worldguard.commands.region.RegionCommands;
 import com.sk89q.worldguard.protection.flags.Flag;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
-
-import net.md_5.bungee.api.chat.BaseComponent;
 import wgextender.Config;
 import wgextender.utils.WGRegionUtils;
 
@@ -138,7 +127,7 @@ public class AutoFlags {
 		}
 	}
 
-	private static final class FakeConsoleComandSender implements ConsoleCommandSender, Actor, LocalPlayer {
+	private static final class FakeConsoleComandSender implements Actor, LocalPlayer {
 		private static World world;
 		private static org.bukkit.Location vector;
 
@@ -153,117 +142,8 @@ public class AutoFlags {
 		}
 
 		@Override
-		public Server getServer() {
-			return Bukkit.getServer();
-		}
-
-		@Override
-		public void sendMessage(String arg0) {
-		}
-
-		@Override
-		public void sendMessage(String[] arg0) {
-		}
-
-		@Override
-		public PermissionAttachment addAttachment(Plugin arg0) {
-			return null;
-		}
-
-		@Override
-		public PermissionAttachment addAttachment(Plugin arg0, int arg1) {
-			return null;
-		}
-
-		@Override
-		public PermissionAttachment addAttachment(Plugin arg0, String arg1, boolean arg2) {
-			return null;
-		}
-
-		@Override
-		public PermissionAttachment addAttachment(Plugin arg0, String arg1, boolean arg2, int arg3) {
-			return null;
-		}
-
-		@Override
-		public Set<PermissionAttachmentInfo> getEffectivePermissions() {
-			return Collections.emptySet();
-		}
-
-		@Override
 		public boolean hasPermission(String arg0) {
 			return true;
-		}
-
-		@Override
-		public boolean hasPermission(Permission arg0) {
-			return true;
-		}
-
-		@Override
-		public boolean isPermissionSet(String arg0) {
-			return true;
-		}
-
-		@Override
-		public boolean isPermissionSet(Permission arg0) {
-			return true;
-		}
-
-		@Override
-		public void recalculatePermissions() {
-		}
-
-		@Override
-		public void removeAttachment(PermissionAttachment arg0) {
-		}
-
-		@Override
-		public boolean isOp() {
-			return true;
-		}
-
-		@Override
-		public void setOp(boolean arg0) {
-		}
-
-		@Override
-		public void abandonConversation(Conversation arg0) {
-		}
-
-		@Override
-		public void abandonConversation(Conversation arg0, ConversationAbandonedEvent arg1) {
-		}
-
-		@Override
-		public void acceptConversationInput(String arg0) {
-		}
-
-		@Override
-		public boolean beginConversation(Conversation arg0) {
-			return false;
-		}
-
-		@Override
-		public boolean isConversing() {
-			return false;
-		}
-
-		@Override
-		public void sendRawMessage(String arg0) {
-		}
-
-		@Override
-		public Spigot spigot() {
-			return new Spigot() {
-				@Override
-				public void sendMessage(BaseComponent component) {
-				}
-
-				@Override
-				public void sendMessage(BaseComponent... components) {
-				}
-			};
 		}
 
 		@Override
